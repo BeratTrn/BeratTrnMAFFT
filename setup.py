ae46f80837f0ac881bcc00b852f565880c7a9b8a
BeratTrn <turanberatr@gmail.com>
2026-04-20 16:18:30 +0300

    PyPI versiyonu güncellendi ve .gitignore eklendi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+# README dosyasının içeriğini okutuyoruz
+with open("README.md", "r", encoding="utf-8") as fh:
+    uzun_aciklama = fh.read()
 
 setup(
     name="BeratTrnMAFFT", 
-    version="0.1.0",
-    author="Berat",
+    version="0.1.1", # VERSİYONU 0.1.1 YAPTIK!
+    author="Berat Turan",
     description="Biyoinformatik dersi MAFFT algoritması dönem projesi",
+    long_description=uzun_aciklama, # README'deki yazıları buraya aktarıyor
+    long_description_content_type="text/markdown", # Yazının formatını belirtiyor
     packages=find_packages(),
     install_requires=[
         "numpy", 
